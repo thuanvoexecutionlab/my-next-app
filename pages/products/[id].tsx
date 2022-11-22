2db8950d01b681777a6fe9ad3bffce2adf785781
ryanvo162 <thuan.vo@executionlab.asia>
2022-11-22 09:26:04 +0700

    Fix meta tag

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -6,48 +6,51 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 
 export interface IProductDetailProps {
-  id: string;
+  data: any;
 }
 
 export default function ProductDetail(props: IProductDetailProps) {
-  const router = useRouter();
-  const { id } = router.query;
-  const [product, setProduct] = useState({
-    _id: "",
-    product_name: "",
-    description: "",
-    price: 0,
-    image_url: "",
-  });
+  const { data } = props;
+  console.log(data);
+  // const router = useRouter();
 
-  useEffect(() => {
-    const getProductDetail = async () => {
-      const response = await getProduct(id as string);
-      if (response.success) {
-        setProduct(response.data);
-      }
-    };
-    getProductDetail();
-  }, [id]);
+  // const { id } = router.query;
+  // const [product, setProduct] = useState({
+  //   _id: "",
+  //   product_name: "",
+  //   description: "",
+  //   price: 0,
+  //   image_url: "",
+  // });
+
+  // useEffect(() => {
+  //   const getProductDetail = async () => {
+  //     const response = await getProduct(id as string);
+  //     if (response.success) {
+  //       setProduct(response.data);
+  //     }
+  //   };
+  //   getProductDetail();
+  // }, [id]);
 
   return (
     <>
       <NextSeo
-        title={product.product_name}
-        description={product.description}
+        title={data?.product_name}
+        description={data?.description}
         canonical="https://www.canonical.ie/"
         openGraph={{
-          title: product.product_name,
-          description: product.description,
+          title: data?.product_name,
+          description: data?.description,
           images: [
             {
-              url: product.image_url,
+              url: data?.image_url,
               width: 800,
               height: 600,
-              alt: product.product_name,
+              alt:  data?.product_name,
             },
           ],
-          site_name: product.product_name,
+          site_name: data?.product_name,
         }}
         twitter={{
           handle: "@handle",
@@ -60,15 +63,15 @@ export default function ProductDetail(props: IProductDetailProps) {
         <Item>
           <div className={"row"}>
             <Image
-              src={product.image_url}
-              alt={product.product_name}
+              src={data?.image_url}
+              alt={data?.product_name}
               width={120}
               height={120}
             />
             <div>
-              <h2>{product.product_name}</h2>
-              <p>${product.price}</p>
-              <p>{product.description}</p>
+              <h2>{data?.product_name}</h2>
+              <p>${data?.price}</p>
+              <p>{data?.description}</p>
             </div>
           </div>
         </Item>
@@ -78,8 +81,13 @@ export default function ProductDetail(props: IProductDetailProps) {
 }
 
 export async function getServerSideProps(context: any) {
-  const { id } = context.query;
-  return {
-    props: { id },
-  };
-}
+  const { id } = context.params;
+  const response = await getProduct(id);
+  if (response.success) {
+    return {
+      props: {
+        data: response.data,
+      },
+    };
+  }
+};
